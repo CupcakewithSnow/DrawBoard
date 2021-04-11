415e5983ab2dddb3ad8ee6b1ba51b1b510ff93e6
Danil D <drujinin.danilll@gmail.com>
2021-04-11 17:26:58 +0700

    realese v1

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Canvas from './component/Canvas'
-import SetBar from './component/SetBar'
+
 import ToolBar from './component/ToolBar'
 import './style/app.scss'
 const App = () => {
   return (
     <div className='app'>
       <ToolBar/>
-      <SetBar/>
+     
       <Canvas/>
     </div>
   )
--- client/src/component/ToolBar.jsx
+++ client/src/component/ToolBar.jsx
@@ -7,21 +7,25 @@ import Quere from '../tools/Quere'
 import Line from '../tools/Line'
 import Eraeser from '../tools/Eraeser'
 import canvasState from '../store/canvasState'
+import toolsState from '../store/toolsState'
 const ToolBar = () => {
     return (
         <div className='toolBar'>
             <div>
-                <button className='toolBar__btn brush' onClick={()=>toolState.setTool(new Brush(canvasState.canvas))}></button>
-                <button className='toolBar__btn line' onClick={()=>toolState.setTool(new Line(canvasState.canvas))}></button>
-                <button className='toolBar__btn circle' onClick={()=>toolState.setTool(new Circle(canvasState.canvas))}></button>
-                <button className='toolBar__btn squer' onClick={()=>toolState.setTool(new Quere(canvasState.canvas))}></button>
-                <button className='toolBar__btn eraeser' onClick={()=>toolState.setTool(new Eraeser(canvasState.canvas))}></button>
-                <input type='color' style={{marginLeft:10}}
-                onChange = {(e)=>toolState.setStrokColor(e.target.value)}
+                <button className='toolBar__btn brush' onClick={() => toolState.setTool(new Brush(canvasState.canvas))}></button>
+                <button className='toolBar__btn line' onClick={() => toolState.setTool(new Line(canvasState.canvas))}></button>
+                <button className='toolBar__btn circle' onClick={() => toolState.setTool(new Circle(canvasState.canvas))}></button>
+                <button className='toolBar__btn squer' onClick={() => toolState.setTool(new Quere(canvasState.canvas))}></button>
+                <button className='toolBar__btn eraeser' onClick={() => toolState.setTool(new Eraeser(canvasState.canvas))}></button>
+                
+                <input type='color' style={{ marginLeft: 10 }}
+                    onChange={(e) => toolState.setStrokColor(e.target.value)}
                 ></input>
+                <label htmlFor='lineWidth'>Толщина линии</label>
+                <input style={{ margin: '0 10px' }} id='lineWidth' type='number' min={1} max={50} defaultValue={1} onChange={e => toolsState.setLineWidth(e.target.value)} />
             </div>
             <div>
-                <button className='toolBar__btn back'></button> 
+                <button className='toolBar__btn back'></button>
                 <button className='toolBar__btn next'></button>
                 <button className='toolBar__btn save'></button>
             </div>
